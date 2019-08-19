45f18eab71c273cd96d3b52967fd41b7c3b8baa1
David Scales <davidsscales@gmail.com>
2019-08-18 20:09:03 -0700

    ref(utils): hoistable functions
    This commit refactors the helpers in util.js to use the classic
    hoistable JavaScript function pattern instead of const+arrow. This
    is needed because our build process just bundles files together in
    a random order. (So a function could be defined in arbitrarty order
    in the final server.js file)

--- dev/lib/util.js
+++ dev/lib/util.js
@@ -1,12 +1,16 @@
-const getRandomInt = (min, max) => {
+/* Global functions in lib/ should should classic syntax instead of
+const+arrow pattern. We need them to be hoisted since the build process
+just bundles the files in random order */
+
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
-const generateId = () => {
+function generateId() {
   return getRandomInt(100000, 999999);
-};
+}
 
 /* Not using module.exports because require() is unavailable in the sandbox environment */
 // module.exports = {
