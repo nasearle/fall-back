213392ca47000806052837649f9e121f5ef04a24
Nicolas Searle <nasearle@ucla.edu>
2019-08-20 11:41:00 -0700

    feat(entities): health, enemy bullets
    This commit adds player and enemy health, hitboxes, and collisions. It adds a getDistance method to the Entity class. For now the enemies just shoot towards a random player appr. once a second.

--- dev/lib/entities.js
+++ dev/lib/entities.js
@@ -8,14 +8,26 @@ class Entity {
     this.y = 250;
     this.speedX = 0;
     this.speedY = 0;
+    // this.lastUpdateTime = new Date().getTime();
   }
   update() {
     this.updatePosition();
   }
   updatePosition() {
+    // TODO: https://hackernoon.com/how-to-build-a-multiplayer-browser-game-4a793818c29b
+    // const currentTime = new Date().getTime();
+    // const timeDifference = currentTime - this.lastUpdateTime;
+    // this.x += this.speedX * timeDifference;
+    // this.y += this.speedY * timeDifference;
     this.x += this.speedX;
     this.y += this.speedY;
+    // this.lastUpdateTime = currentTime;
   }
+  getDistance(point) {
+    return Math.sqrt(
+      Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2)
+    );
+  };
   static getFrameUpdateData() {
     const packs = {
       initPack: {
@@ -55,6 +67,7 @@ class Bullet extends Entity {
     this.speedX = Math.cos((config.angle / 180) * Math.PI) * 10;
     this.speedY = Math.sin((config.angle / 180) * Math.PI) * 10;
     this.timer = 0;
+    this.damage = config.damage; // comes from the Player, based on weapon
     this.toRemove = false;
     Bullet.bullets[this.id] = this;
   }
@@ -63,6 +76,37 @@ class Bullet extends Entity {
       this.toRemove = true;
     }
     super.update();
+    for (let id in Enemy.enemies) {
+      let enemy = Enemy.enemies[id];
+      if (this.getDistance(enemy) < 32 && this.parent !== enemy.id) {
+        enemy.hp -= this.damage;
+        if (enemy.hp <= 0) { // enemy removal handled in Enemy class
+          const shooter = Player.players[this.parent];
+          if (shooter) {
+            shooter.score += 100;
+          }
+        }
+        this.toRemove = true;
+      }
+    }
+    for (let id in Player.players) {
+      let player = Player.players[id];
+      const playerIds = Object.keys(Player.players);
+      // no friendly fire ;)
+      if (this.getDistance(player) < 32 && !playerIds.includes(this.parent)) {
+        // TODO: update hard-coded distance
+        player.hp -= this.damage;
+        if (player.hp <= 0) {
+          player.hp = player.hpMax;
+          player.x = Math.random() * 500;
+          player.y = Math.random() * 500;
+        }
+        this.toRemove = true;
+      }
+    }
+  }
+  setDamage(damage) {
+    this.damage = damage;
   }
   getUpdatePack() {
     return {
@@ -96,13 +140,44 @@ class Enemy extends Entity {
     this.y = -5; // Just beyond top of screen, TODO: should be related to sprite height
     // Keep speed low and march chance high for smoother movement?
     this.maxSpeed = 5;
+    this.hp = 30;
+    this.hpMax = 30;
+    this.damage = 10;
+    this.toRemove = false;
     // Add player to "global" enemies object
     Enemy.enemies[id] = this;
     console.log('New enemy:', this);
   }
   update() {
+    if (this.hp <= 0) {
+      this.toRemove = true;
+    }
     this.updateSpeed();
     super.update();
+    // For now, enemies simply shoot at a player randomly
+    if (Math.random() <= Enemy.chanceToShoot) {
+      console.log(Object.keys(Player.players));
+      console.log(Object.keys(Player.players).length);
+      const numPlayers = Object.keys(Player.players).length;
+      // get a random player
+      const player =
+        Player.players[
+          Object.keys(Player.players)[
+            Math.floor(Math.random() * numPlayers)
+          ]
+        ];
+      let x;
+      let y;
+      if (player) {
+        x = -this.x + player.x - 8; // TODO: replace hard-coded canvas margin
+        y = -this.y + player.y - 8; // TODO: replace hard-coded canvas margin
+      } else {
+        x = -8; // TODO: replace hard-coded canvas margin
+        y = -8 + 1; // TODO: replace hard-coded canvas margin
+      }
+      const angle = (Math.atan2(y, x) / Math.PI) * 180;
+      this.shootBullet(angle);
+    }
   }
   updateSpeed() {
     // For now, enemies simply march down randomly
@@ -112,8 +187,23 @@ class Enemy extends Entity {
       this.speedY = 0;
     }
   }
+  shootBullet(angle) {
+    new Bullet({
+      parent: this.id,
+      angle: angle,
+      x: this.x,
+      y: this.y,
+      damage: this.damage
+    });
+  };
+  getUpdatePack() {
+    return {
+      id: this.id,
+      x: this.x,
+      y: this.y,
+    };
+  }
   static updateEnemies() {
-
     // Periodically generate new enemies
     const numEnemies = Object.keys(Enemy.enemies).length; // could cache this
     if (Math.random() <= Enemy.chanceToGenerate && numEnemies < Enemy.numCap) {
@@ -133,11 +223,12 @@ class Enemy extends Entity {
     for (let id in Enemy.enemies) {
       let enemy = Enemy.enemies[id];
       enemy.update();
-      pack.push({
-        id: enemy.id,
-        x: enemy.x,
-        y: enemy.y,
-      });
+      if (enemy.toRemove) {
+        delete Enemy.enemies[id];
+        removePack.enemies.push(enemy.id);
+      } else {
+        pack.push(enemy.getUpdatePack());
+      }
     }
     return pack;
   }
@@ -145,6 +236,8 @@ class Enemy extends Entity {
 Enemy.enemies = {};
 // Chance is twice per second
 Enemy.chanceToMarch = 2 / FPS;
+// Chance is once per second
+Enemy.chanceToShoot = 1 / FPS;
 // Chance is once per 5 seconds
 Enemy.chanceToGenerate = 1 / (5 * FPS);
 // How fast the *rate* of enemy generation increase: 5% increase every 15 seconds
@@ -163,8 +256,11 @@ class Player extends Entity {
     this.pressingShoot = false;
     this.mouseAngle = 0;
     this.maxSpeed = 10;
-    this.hp = 10;
-    this.hpMax = 10;
+    this.hp = 100;
+    this.hpMax = 100;
+    this.damage = 10;  // can base this on the current weapon / power up
+    this.coolDown = 400;  // can base this on the current weapon / power up
+    this.timeLastShot = 0;
     this.score = 0;
     // Add player to "global" players object
     Player.players[id] = this;
@@ -173,8 +269,12 @@ class Player extends Entity {
     this.updateSpeed();
     super.update();
 
-    if (this.pressingShoot) { // only happens if the click is down during an update loop
+    const currentTime = new Date().getTime();
+    const timeDifference = currentTime - this.timeLastShot;
+    // Note: this.pressingShoot only triggered if the click is down during an update loop
+    if (this.pressingShoot && timeDifference > this.coolDown) {
       this.shootBullet(this.mouseAngle);
+      this.timeLastShot = currentTime;
     }
   }
   updateSpeed() {
@@ -200,6 +300,7 @@ class Player extends Entity {
       angle: angle,
       x: this.x,
       y: this.y,
+      damage: this.damage
     });
   };
   setPressingKey(inputId, state) {
