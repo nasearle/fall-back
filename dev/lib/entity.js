7dfb051f02850d45ba891651c47897e4d09b1c07
David Scales <davidsscales@gmail.com>
2019-08-31 14:54:41 -0700

    overhaul with Game class
    This commit creates "games" so that we can have a large amount of players that aren't all in the same game.
    
    Changes:
    
    * Classes are now pulled into files. These are imported in appropriate order in server.js.
    * Server now loops over game instances, and calls getFrameUpdateData on each of them (instead of on Entity). This updates the entites in each individual game, and then sends that data to the correspoding socket "rooms".
    
    * A Game class is implemented. It's basically a holder of entities and "packs" (initPack, removePack, etc.)
        * Global initPack & removePack are now properties of games (game.removePack, etc.)
        * Class static variables (Player.players, Enemy.enemies, etc.) are now properties of games (game.players, etc.)
    * Games also track socket "rooms"
    * Games are created on player connections, and players are added to existing games until all existing games have 4 players, at which point a new game is created.
    * Some redundant code was cleaned up into loops, e.g:
    
    packs.removePack[player] = this.removePack[player]
    packs.removePack[enemies] = this.removePack[enemies]
    [ ... ]
    
    is now just a loop:
    
    packs.removePack[entityType] = this.removePack[entityType]
    
    * Basically the same, but now Enemy.enemies, Bullet.bullets, etc. have been replaced but game.enemies, game.bullets etc. Because we only want to check collisions and such between entities in the same game
    * Some methods need a gameId now
    
    * Pretty much same as bullet/enemy/obstacle but onConnect now adds player to a socket room + game
    * new method findPlayerById for finding players across games (for example in onDisconnect)
    
    * Moved getAllInitPack into Entity since it was the same for all child classes
    
    * Added some minor helpers for getting keys and stuff
    
    Testing - try opening 5 windows and you will see the fifth player is in a new game by themselves

---
+++ dev/lib/entity.js
@@ -0,0 +1,47 @@
+class Entity {
+    constructor() {
+      this.id = '';
+      this.gameId = '';
+      this.x = 250; //TODO: don't spawn player on obstacle
+      this.y = 250; //TODO: don't spawn player on obstacle
+      this.speedX = 0;
+      this.speedY = 0;
+      // this.lastUpdateTime = new Date().getTime();
+    }
+    update() {
+      this.updatePosition();
+    }
+    updatePosition() {
+      // TODO: https://hackernoon.com/how-to-build-a-multiplayer-browser-game-4a793818c29b
+      // const currentTime = new Date().getTime();
+      // const timeDifference = currentTime - this.lastUpdateTime;
+      // this.x += this.speedX * timeDifference;
+      // this.y += this.speedY * timeDifference;
+      this.x += this.speedX;
+      this.y += this.speedY;
+      // this.lastUpdateTime = currentTime;
+    }
+    // TODO: make static
+    getAngle(point) {
+      const x = -this.x + point.x - 8; // TODO: replace hard-coded canvas margin
+      const y = -this.y + point.y - 8; // TODO: replace hard-coded canvas margin
+      return (Math.atan2(y, x) / Math.PI) * 180;
+    }
+    static overlaps(self, target) {
+      return self.x < target.x + target.width &&
+             self.x + self.width > target.x &&
+             self.y < target.y + target.height &&
+             self.y + self.height > target.y
+    }
+    static getAllInitPack(gameId, entityType) {
+      const existingEntities = [];
+      const game = GAMES[gameId];
+      for (let id in game[entityType]) {
+        const entity = game[entityType][id];
+        existingEntities.push(entity.getInitPack());
+      }
+      return existingEntities;
+    }
+  }
+
+/* Not using module.exports because require() is unavailable in the sandbox environment */
