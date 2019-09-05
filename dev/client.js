3d8fdf013b570d04470165664e1e861326859b00
David Scales <davidsscales@gmail.com>
2019-09-04 18:56:16 -0700

    ref(client): astract Sprite classes

--- dev/client.js
+++ dev/client.js
@@ -5,95 +5,9 @@
     let socket;
 
     kontra.init();
-    const players = {};
-    const enemies = {};
-    const bullets = {};
-    const obstacles = {};
-    const items = {};
 
-    const addPlayer = playerInfo => {
-      const player = kontra.Sprite({
-        type: 'player',
-        id: playerInfo.id,
-        x: playerInfo.x,
-        y: playerInfo.y,
-        width: playerInfo.width,
-        height: playerInfo.height,
-        hp: playerInfo.hp,
-        hpMax: playerInfo.hpMax,
-        score: playerInfo.score,
-        render() {
-          this.context.strokeStyle = 'green';
-          this.context.strokeRect(this.x, this.y, this.width, this.height);
-          // just to see where x & y are in the rect (top left)
-          this.context.fillStyle = 'black';
-          this.context.fillRect(this.x, this.y, 5, 5);
-        },
-      });
-      players[player.id] = player;
-    };
-
-    let image = new Image();
-    image.src = 'assets/alien-32.png';
-    const addEnemy = enemyInfo => {
-      const enemy = kontra.Sprite({
-        type: 'enemy',
-        id: enemyInfo.id,
-        x: enemyInfo.x,
-        y: enemyInfo.y,
-        width: enemyInfo.width,
-        height: enemyInfo.height,
-        hp: enemyInfo.hp,
-        image: image
-      });
-      enemies[enemy.id] = enemy;
-    };
-
-    const addBullet = bulletInfo => {
-      const bullet = kontra.Sprite({
-        type: 'bullet',
-        id: bulletInfo.id,
-        x: bulletInfo.x,
-        y: bulletInfo.y,
-        render() {
-          this.context.fillRect(this.x, this.y, 10, 10);
-        },
-      });
-      bullets[bullet.id] = bullet;
-    };
-
-    const addObstacle = obstacleInfo => {
-      const obstacle = kontra.Sprite({
-        type: 'obstacle',
-        id: obstacleInfo.id,
-        x: obstacleInfo.x,
-        y: obstacleInfo.y,
-        width: obstacleInfo.width,
-        height: obstacleInfo.height,
-        render() {
-          this.context.fillStyle = 'grey';
-          this.context.fillRect(this.x, this.y, this.width, this.height);
-        },
-      });
-      obstacles[obstacle.id] = obstacle;
-    };
-
-    const addItem = itemInfo => {
-      const item = kontra.Sprite({
-        type: 'item',
-        id: itemInfo.id,
-        x: itemInfo.x,
-        y: itemInfo.y,
-        width: itemInfo.width,
-        height: itemInfo.height,
-        color: itemInfo.color,
-        render() {
-          this.context.fillStyle = this.color;
-          this.context.fillRect(this.x, this.y, this.width, this.height);
-        },
-      });
-      items[item.id] = item;
-    };
+    /* The comment below will be replaced with file contents on build */
+    //=require lib/sprites.js
 
     /**
      * Client module init
@@ -107,7 +21,7 @@
       ctx.font = '30px Roboto';
 
       socket.on('newPlayer', playerInfo => {
-        addPlayer(playerInfo);
+        new PlayerSprite(playerInfo);
       });
 
       // will use selfId to access props of this player in the future
@@ -117,19 +31,19 @@
           selfId = data.selfId;
         }
         for (let i = 0; i < data.players.length; i++) {
-          addPlayer(data.players[i]);
+          new PlayerSprite(data.players[i]);
         }
         for (let i = 0; i < data.enemies.length; i++) {
-          addEnemy(data.enemies[i]);
+          new EnemySprite(data.enemies[i]);
         }
         for (let i = 0; i < data.bullets.length; i++) {
-          addBullet(data.bullets[i]);
+          new BulletSprite(data.bullets[i]);
         }
         for (let i = 0; i < data.obstacles.length; i++) {
-          addObstacle(data.obstacles[i]);
+          new ObstacleSprite(data.obstacles[i]);
         }
         for (let i = 0; i < data.items.length; i++) {
-          addItem(data.items[i]);
+          new ItemSprite(data.items[i]);
         }
       });
 
@@ -137,86 +51,86 @@
         const playersData = data.players;
         for (let i = 0; i < playersData.length; i++) {
           let player = playersData[i];
-          players[player.id].x = player.x;
-          players[player.id].y = player.y;
-          players[player.id].hp = player.hp;
-          players[player.id].score = player.score;
+          PlayerSprite.sprites[player.id].x = player.x;
+          PlayerSprite.sprites[player.id].y = player.y;
+          PlayerSprite.sprites[player.id].hp = player.hp;
+          PlayerSprite.sprites[player.id].score = player.score;
         }
         const enemiesData = data.enemies;
         for (let i = 0; i < enemiesData.length; i++) {
           let enemy = enemiesData[i];
-          enemies[enemy.id].x = enemy.x;
-          enemies[enemy.id].y = enemy.y;
-          enemies[enemy.id].hp = enemy.hp;
+          EnemySprite.sprites[enemy.id].x = enemy.x;
+          EnemySprite.sprites[enemy.id].y = enemy.y;
+          EnemySprite.sprites[enemy.id].hp = enemy.hp;
         }
         const bulletsData = data.bullets;
         for (let i = 0; i < bulletsData.length; i++) {
           let bullet = bulletsData[i];
-          bullets[bullet.id].x = bullet.x;
-          bullets[bullet.id].y = bullet.y;
+          BulletSprite.sprites[bullet.id].x = bullet.x;
+          BulletSprite.sprites[bullet.id].y = bullet.y;
         }
         const obstaclesData = data.obstacles;
         for (let i = 0; i < obstaclesData.length; i++) {
           let obstacle = obstaclesData[i];
-          obstacles[obstacle.id].x = obstacle.x;
-          obstacles[obstacle.id].y = obstacle.y;
+          ObstacleSprite.sprites[obstacle.id].x = obstacle.x;
+          ObstacleSprite.sprites[obstacle.id].y = obstacle.y;
         }
         const itemsData = data.items;
         for (let i = 0; i < itemsData.length; i++) {
           let item = itemsData[i];
-          items[item.id].x = item.x;
-          items[item.id].y = item.y;
+          ItemSprite.sprites[item.id].x = item.x;
+          ItemSprite.sprites[item.id].y = item.y;
         }
       });
 
       socket.on('remove', data => {
         for (let i = 0; i < data.players.length; i++) {
-          delete players[data.players[i]];
+          delete PlayerSprite.sprites[data.players[i]];
         }
         for (let i = 0; i < data.enemies.length; i++) {
-          delete enemies[data.enemies[i]];
+          delete EnemySprite.sprites[data.enemies[i]];
         }
         for (let i = 0; i < data.bullets.length; i++) {
-          delete bullets[data.bullets[i]];
+          delete BulletSprite.sprites[data.bullets[i]];
         }
         for (let i = 0; i < data.obstacles.length; i++) {
-          delete obstacles[data.obstacles[i]];
+          delete ObstacleSprite.sprites[data.obstacles[i]];
         }
         for (let i = 0; i < data.items.length; i++) {
-          delete items[data.items[i]];
+          delete ItemSprite.sprites[data.items[i]];
         }
       });
 
       // Use requestAnimationFrame to ensure paints happen performantly
       const renderLoop = () => {
         // For debug
-        const numEnemies = Object.keys(enemies).length;
+        const numEnemies = Object.keys(EnemySprite.sprites).length;
         document.querySelector('span#num-enemies').textContent = numEnemies;
 
-        if (players[selfId]) {
+        if (PlayerSprite.sprites[selfId]) {
           document.querySelector('span#player-score').textContent =
-            players[selfId].score;
+            PlayerSprite.sprites[selfId].score;
         }
 
         ctx.clearRect(0, 0, 500, 500);
-        for (let i in players) {
-          const player = players[i];
+        for (let i in PlayerSprite.sprites) {
+          const player = PlayerSprite.sprites[i];
           player.render();
         }
-        for (let i in enemies) {
-          const enemy = enemies[i];
+        for (let i in EnemySprite.sprites) {
+          const enemy = EnemySprite.sprites[i];
           enemy.render();
         }
-        for (let i in bullets) {
-          const bullet = bullets[i];
+        for (let i in BulletSprite.sprites) {
+          const bullet = BulletSprite.sprites[i];
           bullet.render();
         }
-        for (let i in obstacles) {
-          const obstacle = obstacles[i];
+        for (let i in ObstacleSprite.sprites) {
+          const obstacle = ObstacleSprite.sprites[i];
           obstacle.render();
         }
-        for (let i in items) {
-          const item = items[i];
+        for (let i in ItemSprite.sprites) {
+          const item = ItemSprite.sprites[i];
           item.render();
         }
         window.requestAnimationFrame(renderLoop);
---
+++ dev/lib/sprites.js
@@ -0,0 +1,67 @@
+class PlayerSprite extends kontra.Sprite {
+  constructor(config) {
+    super(config);
+    this.type = "player";
+    this.render = function() {
+      this.context.strokeStyle = "green";
+      this.context.strokeRect(this.x, this.y, this.width, this.height);
+      // just to see where x & y are in the rect (top left)
+      this.context.fillStyle = "black";
+      this.context.fillRect(this.x, this.y, 5, 5);
+    };
+    PlayerSprite.sprites[this.id] = this;
+  }
+}
+PlayerSprite.sprites = {};
+
+class EnemySprite extends kontra.Sprite {
+  constructor(config) {
+    super(config);
+    this.type = "enemy";
+    this.image = EnemySprite.image;
+    EnemySprite.sprites[this.id] = this;
+  }
+}
+EnemySprite.sprites = {};
+EnemySprite.image = new Image();
+EnemySprite.image.src = "assets/alien-32.png";
+
+class BulletSprite extends kontra.Sprite {
+  constructor(config) {
+    super(config);
+    this.type = "bullet";
+    this.render = function() {
+      this.context.fillRect(this.x, this.y, 10, 10);
+    };
+    BulletSprite.sprites[this.id] = this;
+  }
+}
+BulletSprite.sprites = {};
+
+class ObstacleSprite extends kontra.Sprite {
+  constructor(config) {
+    super(config);
+    this.type = "obstacle";
+    this.render = function() {
+      this.context.fillStyle = "grey";
+      this.context.fillRect(this.x, this.y, this.width, this.height);
+    };
+    ObstacleSprite.sprites[this.id] = this;
+  }
+}
+ObstacleSprite.sprites = {};
+
+class ItemSprite extends kontra.Sprite {
+  constructor(config) {
+    super(config);
+    this.type = "item";
+    this.render = function() {
+      this.context.fillStyle = this.color;
+      this.context.fillRect(this.x, this.y, this.width, this.height);
+    };
+    ItemSprite.sprites[this.id] = this;
+  }
+}
+ItemSprite.sprites = {};
+
+/* Not using module.exports because require() is unavailable in the sandbox environment */
